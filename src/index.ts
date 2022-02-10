8a4526c30617c1c73241458dc7269573086f1cb1
Miora RAKOTONDRABARY <rakotondrabarymiora@gmail.com>
2022-02-10 17:08:46 +0100

    steps 1 2 3  and 4

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import nunjucks from "nunjucks";
+import cookie from "cookie";
+
+const app = express();
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+});
+
+app.set("view engine", "njk");
+
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.render("home");
+});
+
+app.get("/options", (request, response) => {
+  response.render("options");
+});
+
+const formParser = express.urlencoded({ extended: true });
+
+app.post("/home", formParser, (request, response) => {
+  const themeChoice = request.body.theme;
+  response.set(
+    "Set-Cookie",
+    cookie.serialize("myCookie", themeChoice, {
+      maxAge: 3600,
+    }),
+  );
+  response.render("home");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
+});
